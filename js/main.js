7632298d06a3d5a89f0e11e394b29308837f5023
Jose Manuel <jose.garcesm@upb.edu.co>
2021-10-18 01:28:19 -0500

    API 6

--- js/main.js
+++ js/main.js
@@ -1,32 +1,33 @@
-deja pokemon1, pokemon2;
-let name1, name2;
-deja ataque1, ataque2;
-dejemos defensa1, defesensa2;
+deja poke1, poke2;
+deja tip1, tip2;
+let nomb1, nomb2;
+deja ataq1, ataq2;
+dejemos defen1, defen2;
 funcion batalla (){
-  let aleatorio1 = Math.round (Math.random () * (150-1) + parseInt (1)) ;;
-  let aleatorio2 = Math.round (Math.random () * (150-2) + parseInt (1)) ;;
-  p1 = document.getElementById ("img1");
-  p2 = document.getElementById ("img2");
-  nombre1 = document.getElementById ("nombre1");
-  nombre2 = document.getElementById ("nombre2");
-  tipo1 = document.getElementById ("tipo1");
-  tipo2 = document.getElementById ("tipo2");
-  ataque1 = document.getElementById ("ataque1");
-  ataque2 = document.getElementById ("ataque2");
-  defensa1 = document.getElementById ("defensa1");
-  defensa2 = document.getElementById ("defensa2");
-  traerDatos (p1, nombre1, tipo1, ataque1, defensa1, aleatorio1);
-  traerDatos (p2, nombre2, tipo2, ataque2, defensa2, aleatorio2);
+  let aleatorio1 = Math.round (Math.random () * (100-1) + parseInt (1)) ;;
+  let aleatorio2 = Math.round (Math.random () * (100-2) + parseInt (1)) ;;
+  poke1 = document.getElementById ("img1");
+  poke2 = document.getElementById ("img2");
+  nomb1 = document.getElementById ("nombre1");
+  nomb2 = document.getElementById ("nombre2");
+  tip1 = document.getElementById ("tipo1");
+  tip2 = document.getElementById ("tipo2");
+  ataq1 = document.getElementById ("ataque1");
+  ataq2 = document.getElementById ("ataque2");
+  defen1 = document.getElementById ("defensa1");
+  defen2 = document.getElementById ("defensa2");
+  traerDatos (poke1, nomb1, tip1, ataq1, defen1, aleatorio1);
+  traerDatos (poke2, nomb2, tip2, ataq2, defen2, aleatorio2);
   fondo (aleatorio1);
   fondo (aleatorio2);
   }
   function traerDatos (contenedor, nombre, tipo, ataque, defensa, id) {
-    buscar (`https://pokeapi.co/api/v2/pokemon/ {id}`)
+    buscar (`https://pokeapi.co/api/v2/pokemon/$ {id}`)
     .entonces (respuesta => respuesta.json ())
     .entonces (datos => {
       contenedor.src = data.sprites.front_default;
       nombre.innerHTML = data.name;
-      tipo.innerHTML = "Tipo" + data.stats [1] .base_stat;
+      tipo.innerHTML = "Tipo:" + data.stats [1] .base_stat;
       ataque.innerHTML = "Ataque:" + data.stats [2] .base_stat;
       defensa.innerHTML = "Defensa:" + data.stats [3] .base_stat;
      })
