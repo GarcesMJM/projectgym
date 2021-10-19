b680243f1ba5a3e63fa3cdf21e692e5841b279a3
Jose Manuel <jose.garcesm@upb.edu.co>
2021-10-19 09:49:41 -0500

    API 8

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-dejapoke1, poke2;
-dejatip1, tip2;
+deja poke1, poke2;
+deja tip1, tip2;
 let nomb1, nomb2;
-dejaataq1, ataq2;
+deja ataq1, ataq2;
 let dejemosdefen1, defen2;
-let funcionbatalla 
+let funcionbatalla ;
   let aleatorio1 = Math.round (Math.random () * (100-1) + parseInt (1)) ;;
   let aleatorio2 = Math.round (Math.random () * (100-2) + parseInt (1)) ;;
   poke1 = document.getElementById ("img1");
@@ -18,13 +18,7 @@ let funcionbatalla
   defen2 = document.getElementById ("defensa2");
   traerDatos (poke1, nomb1, tip1, ataq1, defen1, aleatorio1);
   traerDatos (poke2, nomb2, tip2, ataq2, defen2, aleatorio2);
-<<<<<<< HEAD
-  }
-=======
-  fondo (aleatorio1);
-  fondo (aleatorio2);
-  
->>>>>>> 9549246e38e64854a3cf9a68276c80725f4873b2
+
   function traerDatos (contenedor, nombre, tipo, ataque, defensa, id) {
     buscar (`https://pokeapi.co/api/v2/pokemon/$ {id}`)
     .entonces (respuesta => respuesta.json ())
