ac496b3832d0ebf4a5d74896255b5b8dcd86ede0
Oscar Andrés Castaño <89107253+Osicasta@users.noreply.github.com>
2021-10-19 23:20:40 -0500

    API "version 1" :)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-deja poke1, poke2;
-deja tip1, tip2;
+let poke1, poke2;
+let tip1, tip2;
 let nomb1, nomb2;
-deja ataq1, ataq2;
-deja defen1, defen2;
+let ataq1, ataq2;
+let defen1, defen2;
+
+
 function batalla(){
   let aleatorio1 = Math.round (Math.random () * (100-1) + parseInt (1)) ;;
   let aleatorio2 = Math.round (Math.random () * (100-2) + parseInt (1)) ;;
@@ -10,25 +12,17 @@ function batalla(){
   poke2 = document.getElementById ("img2");
   nomb1 = document.getElementById ("nombre1");
   nomb2 = document.getElementById ("nombre2");
-  tip1 = document.getElementById ("tipo1");
-  tip2 = document.getElementById ("tipo2");
-  ataq1 = document.getElementById ("ataque1");
-  ataq2 = document.getElementById ("ataque2");
-  defen1 = document.getElementById ("defensa1");
-  defen2 = document.getElementById ("defensa2");
-  traerDatos (poke1, nomb1, tip1, ataq1, defen1, aleatorio1);
-  traerDatos (poke2, nomb2, tip2, ataq2, defen2, aleatorio2);
+
+  traerDatos (poke1,nomb1,aleatorio1);
+  traerDatos (poke2,nomb2,aleatorio2);
 
 }
   
-  function traerDatos (contenedor, nombre, tipo, ataque, defensa, id) {
-    buscar (`https://pokeapi.co/api/v2/pokemon/$ {id}`)
-    .entonces (respuesta => respuesta.json ())
-    .entonces (datos => {
-      contenedor.src = data.sprites.front_default;
+  function traerDatos (contenedor, nombre,id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then (response => response.json ())
+    .then (data => {
+      contenedor.src = data.sprites.other.dream_world.front_default; 
       nombre.innerHTML = data.name;
-      tipo.innerHTML = "Tipo:" + data.stats [1] .base_stat;
-      ataque.innerHTML = "Ataque:" + data.stats [2] .base_stat;
-      defensa.innerHTML = "Defensa:" + data.stats [3] .base_stat;
      })
 }
